bf608b170ec75d15953ecd6c83003ab6adb62bd0
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-16 22:53:04 +0200

    Implement fetch from github

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 
+from fastapi import HTTPException, status
 from app import app, httpx_client, ENVIRONMENT
 
 
@@ -12,9 +13,21 @@ def index_route():
 
 @app.get('/{username}/{repository}')
 async def get_commits(username, repository):
-    response = await httpx_client.get("/repos/{username}/{repository}/commits")
+    response = await httpx_client.get(f"/repos/{username}/{repository}/commits")
+    if response.status_code == 404:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"no repository @ 'github.com/{username}/{repository}'",
+        )
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal Server Error",
+        )
+
     return {
         "username": username,
         "repository": repository,
-        "response": response.content
+        "response": response.content,
+        "status_code": response.status_code
     }
