b63d866eb7d0cefcfccfe8d287988b7360af2e11
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-18 12:55:34 +0200

    Move generate_subdir_zip into utilities

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ from fastapi.responses import RedirectResponse
 
 from app import (ENVIRONMENT, app, fetch_new_token, github_api_client,
                  github_download_client, repo_collection, storage_client)
-from app.utilities import get_default_branch, get_current_sha
+from app.utilities import get_default_branch, get_current_sha, generate_subdir_zip
 import os
 
 
@@ -44,53 +44,7 @@ async def download_subdirectory(username, repository, file_path: str):
     filename = f"{repository}-{encoded_file_path}"
 
     if (db_document is None) or (db_document["sha"] != current_sha):
-
-        default_branch = await get_default_branch(username, repository)
-        tmp_dir = f"tmp/{username}/{repository}/{filename}"
-        if username not in os.listdir("tmp"):
-            os.mkdir(f"tmp/{username}")
-        if repository not in os.listdir(f"tmp/{username}"):
-            os.mkdir(f"tmp/{username}/{repository}")
-        if filename not in os.listdir((f"tmp/{username}/{repository}")):
-            os.mkdir(tmp_dir)
-
-        with open(f"{tmp_dir}/{default_branch}.zip", "wb") as file:
-            response = await github_download_client.get(
-                f"/{username}/{repository}/archive/{default_branch}.zip"
-            )
-            file.write(response.content)
-
-        with ZipFile(f"{tmp_dir}/{default_branch}.zip", 'r') as zip:
-            if f"{repository}-{default_branch}/" not in zip.namelist():
-                raise HTTPException(
-                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail=f"GitHub API did not respond as expected",
-                )
-            zip.extractall(tmp_dir)
-
-        # writing files to a zipfile
-        with ZipFile(f"{tmp_dir}/{filename}.zip", 'w') as zip:
-            os.chdir(f"{tmp_dir}/{repository}-{default_branch}/{file_path}/..")
-            folder_name = file_path.split('/')[-1]
-            file_list = os.listdir(folder_name)
-            for file in file_list:
-                zip.write(f"{folder_name}/{file}")
-            os.chdir("../" * (4 + len(file_path.split('/'))))
-
-        response = storage_client.put(
-            f"/{username}/{repository}/{filename}.zip",
-            data=open(f"{tmp_dir}/{filename}.zip", 'rb')
-        )
-        if response.status != 200:
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"IBM Storage API did not respond as expected",
-            )
-
-        shutil.rmtree(f"{tmp_dir}")
-        if len(os.listdir(f"tmp/{username}/{repository}")) == 0:
-            shutil.rmtree(f"tmp/{username}")
-
+        await generate_subdir_zip(username, repository, file_path, filename)
         await repo_collection.update_one(
             {"url": url}, {"$set": {"sha": current_sha}}, upsert=True
         )
--- app/utilities.py
+++ app/utilities.py
@@ -1,7 +1,10 @@
 
 import json
+import os
+import shutil
+from zipfile import ZipFile
 from fastapi import HTTPException, status
-from app import github_api_client
+from app import github_api_client, github_download_client, storage_client
 
 
 async def get_default_branch(username, repository):
@@ -46,3 +49,52 @@ async def get_current_sha(username, repository, file_path):
         )
 
     return response_dict[0]["sha"]
+
+
+async def generate_subdir_zip(username, repository, file_path, filename):
+    default_branch = await get_default_branch(username, repository)
+    tmp_dir = f"tmp/{username}/{repository}/{filename}"
+    if username not in os.listdir("tmp"):
+        os.mkdir(f"tmp/{username}")
+    if repository not in os.listdir(f"tmp/{username}"):
+        os.mkdir(f"tmp/{username}/{repository}")
+    if filename not in os.listdir((f"tmp/{username}/{repository}")):
+        os.mkdir(tmp_dir)
+
+    with open(f"{tmp_dir}/{default_branch}.zip", "wb") as file:
+        response = await github_download_client.get(
+            f"/{username}/{repository}/archive/{default_branch}.zip"
+        )
+        file.write(response.content)
+
+    with ZipFile(f"{tmp_dir}/{default_branch}.zip", 'r') as zip:
+        if f"{repository}-{default_branch}/" not in zip.namelist():
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"GitHub API did not respond as expected",
+            )
+        zip.extractall(tmp_dir)
+
+    # writing files to a zipfile
+    with ZipFile(f"{tmp_dir}/{filename}.zip", 'w') as zip:
+        os.chdir(f"{tmp_dir}/{repository}-{default_branch}/{file_path}/..")
+        folder_name = file_path.split('/')[-1]
+        file_list = os.listdir(folder_name)
+        for file in file_list:
+            zip.write(f"{folder_name}/{file}")
+        os.chdir("../" * (4 + len(file_path.split('/'))))
+
+    # File upload are not asyncronous
+    response = storage_client.put(
+        f"/{username}/{repository}/{filename}.zip",
+        data=open(f"{tmp_dir}/{filename}.zip", 'rb')
+    )
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"IBM Storage API did not respond as expected",
+        )
+
+    shutil.rmtree(f"{tmp_dir}")
+    if len(os.listdir(f"tmp/{username}/{repository}")) == 0:
+        shutil.rmtree(f"tmp/{username}")
