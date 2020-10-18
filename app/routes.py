fc0e117b1fb08372603b44ce1e370f8617fb3da7
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-18 10:50:27 +0200

    Add distinct routes for subdirectory and root

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 
 from fastapi import HTTPException, status
+from fastapi.responses import RedirectResponse
 from app import app, ENVIRONMENT, repo_collection, \
     github_client, storage_client, fetch_new_token
+import json
 
 
 @app.get('/')
@@ -12,13 +14,25 @@ def index_route():
     }
 
 
-@app.get('/{username}/{repository}')
-async def get_commits(username, repository):
-    response = await github_client.get(f"/repos/{username}/{repository}/commits")
+@app.get('/{username}/{repository}/{file_path:path}')
+async def download_subdirectory(username, repository, file_path: str):
+    try:
+        assert(len(file_path) > 0)
+        assert(file_path[0] != "/")
+    except AssertionError:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details=f"invalid file_path '{file_path}'"
+        )
+
+    response = await github_client.get(
+        f"/repos/{username}/{repository}/commits",
+        params={"path": file_path}
+    )
     if response.status_code == 404:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"no repository @ 'github.com/{username}/{repository}'",
+            detail=f"nothing found @ '/{username}/{repository}/{path}'",
         )
     if response.status_code != 200:
         raise HTTPException(
@@ -26,8 +40,11 @@ async def get_commits(username, repository):
             detail=f"Internal Server Error",
         )
 
-    url = f'/{username}/{repository}'
-    current_sha = response.content["sha"]
+    # return response.content
+
+    url = f'/{username}/{repository}/{file_path}'
+    response_dict = json.loads(response.content)
+    current_sha = response_dict[0]["sha"]
     db_document = await repo_collection.find_one(
         {"url": url},
         {"_id": 0, "sha": 1}
@@ -40,6 +57,7 @@ async def get_commits(username, repository):
         )
 
     if update_zip:
+        # open("tmp/{username}/{repository}")
         # TODO: renew stored file to be downloaded
         pass
 
@@ -48,6 +66,14 @@ async def get_commits(username, repository):
     return {
         "username": username,
         "repository": repository,
-        "response": response.content,
+        "response_dict": response_dict,
         "status_code": response.status_code
     }
+
+
+@app.get('/{username}/{repository}')
+def download_root(username, repository):
+    return RedirectResponse(
+        f"https://github.com/{username}/{repository}" +
+        "/archive/master.zip"
+    )
